e83d7f4181917c46c7ea365428d885bb157fa1d9
Razie Mat Saman <rsaman@indium.com>
2025-05-20 17:31:57 +0800

    Initial database release

---
+++ SDSFinder/EFModels/Document.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SDSFinder.EFModels;
+
+[Table("Document")]
+public partial class Document
+{
+    [Key]
+    public int DocumentId { get; set; }
+
+    [StringLength(100)]
+    public string FileName { get; set; } = null!;
+
+    [StringLength(300)]
+    public string? FileLocation { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreatedDate { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public int? ModifiedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? ModifiedDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
